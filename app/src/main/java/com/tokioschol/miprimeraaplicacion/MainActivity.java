844c649541c43d58c4aaf7c4602a7c0ad65b4ddb
javij <javijimenezhernandez98@gmail.com>
2021-09-18 16:26:38 +0200

    nuevo comit activity

---
+++ app/src/main/java/com/tokioschol/miprimeraaplicacion/MainActivity.java
@@ -0,0 +1,36 @@
+package com.tokioschol.miprimeraaplicacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final  String TAG = "MainActivity";
+    private int numero = 4;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        MiModelo modelo = new MiModelo();
+
+
+
+
+        Log.e(" javi_error", "mensaje de error");
+        Log.e(TAG, "otro mensaje de error");
+
+        Log.i(TAG, "Este mensaje es de informacion: " +numero);
+
+        Log.w(TAG, "esto es un warning");
+
+        Log.d(TAG, "este es el debug");
+
+        Log.d(TAG, "este es el debug");
+
+
+    }
+}
